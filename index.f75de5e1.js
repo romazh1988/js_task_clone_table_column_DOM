a15f4935263b1fda47eea6e393e677cb4ce696cc
Roma Zuravliov <janmorozov1988@gmail.com>
2024-07-21 15:30:00 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const table = document.querySelector("table");
+function cloneAndInsertColumn(section) {
+    for (const row of section.rows){
+        const cloneCell = row.cells[1].cloneNode(true);
+        row.insertBefore(cloneCell, row.cells[row.cells.length - 1]);
+    }
+}
+if (table.tHead) cloneAndInsertColumn(table.tHead);
+if (table.tBodies.length > 0) for (const tbody of table.tBodies)cloneAndInsertColumn(tbody);
+if (table.tFoot) cloneAndInsertColumn(table.tFoot);
+
+//# sourceMappingURL=index.f75de5e1.js.map
